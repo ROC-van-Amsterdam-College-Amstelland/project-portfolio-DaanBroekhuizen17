1d82da0f6cde9b663af1215bb6e6fe11dd755d84
Daan Broekhuizen <daanbroekhuizen@live.com>
2019-11-05 22:33:53 +0100

    bijna klaar met javascript voor projecten pagina

---
+++ javascript/projecten.js
@@ -0,0 +1,260 @@
+function duitsland(){
+    document.getElementById("bovenkant").style.backgroundColor = "black";
+    document.getElementById("middenkant").style.backgroundColor = "red";
+    document.getElementById("onderkant").style.backgroundColor = "gold";
+
+    document.getElementById("bovenkant").style.display = "block";
+    document.getElementById("middenkant").style.display = "block";
+    document.getElementById("onderkant").style.display = "block";
+
+    document.getElementById("bovenkant").style.height = "50px";
+    document.getElementById("middenkant").style.height = "50px";
+    document.getElementById("onderkant").style.height = "50px";
+
+    document.getElementById("middenkant").style.borderBottom = "0px";
+    document.getElementById("middenkant").style.borderTop = "0px";
+
+    document.getElementById("bovenkant").style.width = "450px";
+    document.getElementById("middenkant").style.width = "450px";
+    document.getElementById("onderkant").style.width = "450px";
+    
+    document.getElementById("container").style.backgroundImage = "url('none')"
+    document.getElementById("middenkant").style.borderRadius = "0px";
+    document.getElementById("middenkant").style.marginLeft = "0px";
+    document.getElementById("middenkant").style.marginTop = "0px";
+}
+
+function nederland(){
+    document.getElementById("bovenkant").style.backgroundColor = "red";
+    document.getElementById("middenkant").style.backgroundColor = "white";
+    document.getElementById("onderkant").style.backgroundColor = "blue";
+
+    document.getElementById("bovenkant").style.display = "block";
+    document.getElementById("middenkant").style.display = "block";
+    document.getElementById("onderkant").style.display = "block";
+
+    document.getElementById("bovenkant").style.height = "50px";
+    document.getElementById("middenkant").style.height = "50px";
+    document.getElementById("onderkant").style.height = "50px";
+
+    document.getElementById("middenkant").style.borderBottom = "0px";
+    document.getElementById("middenkant").style.borderTop = "0px";
+
+    document.getElementById("bovenkant").style.width = "450px";
+    document.getElementById("middenkant").style.width = "450px";
+    document.getElementById("onderkant").style.width = "450px";
+    document.getElementById("container").style.backgroundImage = "url('none')"
+    document.getElementById("middenkant").style.borderRadius = "0px";
+    document.getElementById("middenkant").style.marginLeft = "0px";
+    document.getElementById("middenkant").style.marginTop = "0px";
+}
+
+function polen(){
+    document.getElementById("bovenkant").style.backgroundColor = "transparent";
+    document.getElementById("middenkant").style.backgroundColor = "white";
+    document.getElementById("onderkant").style.backgroundColor = "red";
+
+    document.getElementById("bovenkant").style.height = "0px";
+    document.getElementById("middenkant").style.height = "75px";
+    document.getElementById("onderkant").style.height = "75px";
+
+    document.getElementById("bovenkant").style.width = "0px";
+    document.getElementById("middenkant").style.width = "450px";
+    document.getElementById("onderkant").style.width = "450px";
+
+    document.getElementById("middenkant").style.borderBottom = "0px";
+    document.getElementById("middenkant").style.borderTop = "0px";
+
+    document.getElementById("bovenkant").style.display = "block";
+    document.getElementById("middenkant").style.display = "block";
+    document.getElementById("onderkant").style.display = "block";
+    document.getElementById("container").style.backgroundImage = "url('none')"
+    document.getElementById("middenkant").style.borderRadius = "0px";
+    document.getElementById("middenkant").style.marginLeft = "0px";
+    document.getElementById("middenkant").style.marginTop = "0px";
+}
+
+function colombia(){
+    document.getElementById("bovenkant").style.backgroundColor = "gold";
+    document.getElementById("middenkant").style.backgroundColor = "blue";
+    document.getElementById("onderkant").style.backgroundColor = "red";
+
+    document.getElementById("bovenkant").style.width = "450px";
+    document.getElementById("middenkant").style.width = "450px";
+    document.getElementById("onderkant").style.width = "450px";
+
+    document.getElementById("bovenkant").style.height = "80px";
+    document.getElementById("middenkant").style.height = "35px";
+    document.getElementById("onderkant").style.height = "35px";
+
+    document.getElementById("middenkant").style.borderBottom = "0px";
+    document.getElementById("middenkant").style.borderTop = "0px";
+
+    document.getElementById("bovenkant").style.display = "block";
+    document.getElementById("middenkant").style.display = "block";
+    document.getElementById("onderkant").style.display = "block";
+    document.getElementById("container").style.backgroundImage = "url('none')"
+    document.getElementById("middenkant").style.borderRadius = "0px";
+    document.getElementById("middenkant").style.marginLeft = "0px";
+    document.getElementById("middenkant").style.marginTop = "0px";
+}
+
+function botswana(){
+    document.getElementById("bovenkant").style.backgroundColor = "rgb(116, 152, 250)";
+    document.getElementById("middenkant").style.backgroundColor = "black";
+    document.getElementById("onderkant").style.backgroundColor = "rgb(116, 152, 250)";
+
+    document.getElementById("bovenkant").style.width = "450px";
+    document.getElementById("middenkant").style.width = "450px";
+    document.getElementById("onderkant").style.width = "450px";
+
+    document.getElementById("bovenkant").style.height = "57px";
+    document.getElementById("middenkant").style.height = "16px";
+    document.getElementById("onderkant").style.height = "57px";
+
+    document.getElementById("middenkant").style.borderBottom = "10px solid white"
+    document.getElementById("middenkant").style.borderTop = "10px solid white"
+
+    document.getElementById("bovenkant").style.display = "block";
+    document.getElementById("middenkant").style.display = "block";
+    document.getElementById("onderkant").style.display = "block";
+    document.getElementById("container").style.backgroundImage = "url('none')"
+    document.getElementById("middenkant").style.borderRadius = "0px";
+    document.getElementById("middenkant").style.marginLeft = "0px";
+    document.getElementById("middenkant").style.marginTop = "0px";
+}
+
+function denemarken(){
+    document.getElementById("bovenkant").style.backgroundColor = "transparent";
+    document.getElementById("middenkant").style.backgroundColor = "transparent";
+    document.getElementById("onderkant").style.backgroundColor = "transparent";
+
+    document.getElementById("bovenkant").style.width = "450px";
+    document.getElementById("middenkant").style.width = "450px";
+    document.getElementById("onderkant").style.width = "450px";
+
+    document.getElementById("bovenkant").style.height = "50px";
+    document.getElementById("middenkant").style.height = "50px";
+    document.getElementById("onderkant").style.height = "50px";
+
+    document.getElementById("middenkant").style.borderBottom = "0px";
+    document.getElementById("middenkant").style.borderTop = "0px";
+
+    document.getElementById("bovenkant").style.display = "block";
+    document.getElementById("middenkant").style.display = "block";
+    document.getElementById("onderkant").style.display = "block";
+
+    document.getElementById("container").style.backgroundImage = "url('denemarken.png')"
+    document.getElementById("container").style.backgroundPosition = "center center"
+    document.getElementById("middenkant").style.borderRadius = "0px";
+    document.getElementById("middenkant").style.marginLeft = "0px";
+    document.getElementById("middenkant").style.marginTop = "0px";
+}
+
+function noorwegen(){
+    document.getElementById("bovenkant").style.backgroundColor = "transparent";
+    document.getElementById("middenkant").style.backgroundColor = "transparent";
+    document.getElementById("onderkant").style.backgroundColor = "transparent";
+
+    document.getElementById("bovenkant").style.width = "900px";
+    document.getElementById("middenkant").style.width = "900px";
+    document.getElementById("onderkant").style.width = "900px";
+
+    document.getElementById("bovenkant").style.height = "200px";
+    document.getElementById("middenkant").style.height = "200px";
+    document.getElementById("onderkant").style.height = "200px";
+
+    document.getElementById("middenkant").style.borderBottom = "0px";
+    document.getElementById("middenkant").style.borderTop = "0px";
+
+    document.getElementById("bovenkant").style.display = "block";
+    document.getElementById("middenkant").style.display = "block";
+    document.getElementById("onderkant").style.display = "block";
+
+    document.getElementById("container").style.backgroundImage = "url('noorwegen.png')"
+    document.getElementById("container").style.backgroundPosition = "center center"
+    document.getElementById("middenkant").style.borderRadius = "0px";
+    document.getElementById("middenkant").style.marginLeft = "0px";
+    document.getElementById("middenkant").style.marginTop = "0px";
+}
+
+function japan(){
+    document.getElementById("bovenkant").style.backgroundColor = "transparent";
+    document.getElementById("middenkant").style.backgroundColor = "red";
+    document.getElementById("onderkant").style.backgroundColor = "transparent";
+    document.getElementById("container").style.backgroundColor = "white";
+
+    document.getElementById("bovenkant").style.width = "900px";
+    document.getElementById("middenkant").style.width = "300px";
+    document.getElementById("onderkant").style.width = "900px";
+
+    document.getElementById("middenkant").style.borderRadius = "300px";
+    document.getElementById("middenkant").style.marginLeft = "300px";
+    document.getElementById("middenkant").style.marginTop = "-50px";
+
+    document.getElementById("bovenkant").style.height = "200px";
+    document.getElementById("middenkant").style.height = "300px";
+    document.getElementById("onderkant").style.height = "200px";
+
+    document.getElementById("middenkant").style.borderBottom = "0px";
+    document.getElementById("middenkant").style.borderTop = "0px";
+
+    document.getElementById("bovenkant").style.display = "block";
+    document.getElementById("middenkant").style.display = "block";
+    document.getElementById("onderkant").style.display = "block";
+    document.getElementById("container").style.backgroundImage = "url('none')"
+}
+
+function congo(){
+    document.getElementById("bovenkant").style.backgroundColor = "transparent";
+    document.getElementById("middenkant").style.backgroundColor = "transparent";
+    document.getElementById("onderkant").style.backgroundColor = "transparent";
+
+    document.getElementById("bovenkant").style.width = "900px";
+    document.getElementById("middenkant").style.width = "900px";
+    document.getElementById("onderkant").style.width = "900px";
+
+    document.getElementById("bovenkant").style.height = "200px";
+    document.getElementById("middenkant").style.height = "200px";
+    document.getElementById("onderkant").style.height = "200px";
+
+    document.getElementById("middenkant").style.borderBottom = "0px";
+    document.getElementById("middenkant").style.borderTop = "0px";
+
+    document.getElementById("bovenkant").style.display = "block";
+    document.getElementById("middenkant").style.display = "block";
+    document.getElementById("onderkant").style.display = "block";
+
+    document.getElementById("container").style.backgroundImage = "url('congo.png')"
+    document.getElementById("container").style.backgroundPosition = "center center"
+    document.getElementById("middenkant").style.borderRadius = "0px";
+    document.getElementById("middenkant").style.marginLeft = "0px";
+    document.getElementById("middenkant").style.marginTop = "0px";
+}
+
+function zwitserland(){
+    document.getElementById("bovenkant").style.backgroundColor = "transparent";
+    document.getElementById("middenkant").style.backgroundColor = "transparent";
+    document.getElementById("onderkant").style.backgroundColor = "transparent";
+
+    document.getElementById("bovenkant").style.width = "900px";
+    document.getElementById("middenkant").style.width = "900px";
+    document.getElementById("onderkant").style.width = "900px";
+
+    document.getElementById("bovenkant").style.height = "200px";
+    document.getElementById("middenkant").style.height = "200px";
+    document.getElementById("onderkant").style.height = "200px";
+
+    document.getElementById("middenkant").style.borderBottom = "0px";
+    document.getElementById("middenkant").style.borderTop = "0px";
+
+    document.getElementById("bovenkant").style.display = "block";
+    document.getElementById("middenkant").style.display = "block";
+    document.getElementById("onderkant").style.display = "block";
+
+    document.getElementById("container").style.backgroundImage = "url('zwitserland.png')"
+    document.getElementById("container").style.backgroundPosition = "center center"
+    document.getElementById("middenkant").style.borderRadius = "0px";
+    document.getElementById("middenkant").style.marginLeft = "0px";
+    document.getElementById("middenkant").style.marginTop = "0px";
+}
